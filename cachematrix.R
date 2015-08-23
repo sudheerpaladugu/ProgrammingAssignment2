292a131dbebf9e6ee2c82b1772ca1aa8177e0a23
sudheerpaladugu <sudheerpaladugu@yahoo.com>
2015-08-22 21:16:13 -0500

    Updated two operations
    Updated "makeCacheMatrix" and "cacheSolve" operations with code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix function takes martrix input 
+## Operations
+## set - stores input matrix in cache
+## setmatrix - stores input value (m) in cache
+## get - Returns matrix from Cache
+## getmatrix - Returns stored value (m) from cache
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    
+    set<-function(y)
+    {
+        x<<-y
+        m<<-NULL
+    }
+    
+    get<-function() x
+    
+    setmatrix<-function(solve) m<<- solve
+    
+    getmatrix<-function() m
+    
+    list(set=set, get=get,
+         setmatrix=setmatrix,
+         getmatrix=getmatrix)
+    
 }
 
 
-## Write a short comment describing this function
-
+## This function computes inverse matrix and returns it
+## it returns cached inverse matrix if it exist. Otherwise it will recompute
+## inverse matrix of in put matrix, store it in cache, and returns it.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$getmatrix()
+    
+    if(!is.null(m))
+    {
+        message("Getting inversed matrix from cache...")
+        return(m)
+    }
+    matrix<-x$get()
+    
+    m<-solve(matrix, ...)
+    
+    x$setmatrix(m)
+    
+    m
 }
